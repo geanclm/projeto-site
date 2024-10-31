61c2a8b98460ebe25877cf86feca74bfb1263850
Gean Carlos <geanclm@gmail.com>
2024-10-31 06:06:13 -0300

    Update main.py
    Alteração feita no editor do GitHub

--- estudos/projetos-python/01-básico/main.py
+++ estudos/projetos-python/01-básico/main.py
@@ -2,3 +2,4 @@ print('arquivo principal do projeto Python')
 
 # print a text with color green 
 print("\033[92m Texto verde \033[0m")
+print("\033[92m Texto verde - GitHub \033[0m") # linha inserida no editor do GitHub
