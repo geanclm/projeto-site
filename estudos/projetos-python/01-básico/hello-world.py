dab92e1e6838d11911d6f478d0683af0acb827eb
Gean Carlos <geanclm@gmail.com>
2024-11-06 11:34:11 -0300

    update

--- estudos/projetos-python/01-básico/hello-world.py
+++ estudos/projetos-python/01-básico/hello-world.py
@@ -1,3 +1,11 @@
 texto = 'Hello, World!'
 print(texto)
 
+'criar uma função para gerar testar números se um número é primo'
+def eh_primo(n):
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+print(eh_primo(11))
